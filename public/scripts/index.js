29744813f3b86604aab8f1a3882baf0399feea1a
RealDangTan <11236199@st.neu.edu.vn>
2025-04-15 23:40:48 +0700

    Add files via upload

---
+++ public/scripts/index.js
@@ -0,0 +1,17 @@
+// components.js
+function loadComponent(selector, file) {
+    fetch(file)
+      .then(res => res.text())
+      .then(html => {
+        document.querySelector(selector).innerHTML = html;
+      })
+      .catch(err => console.error(`Error loading ${file}:`, err));
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("#navigation", "../components/navigation.html");
+    loadComponent("#footer", "../components/footer.html");
+    loadComponent("#content-card", "../components/content-card.html");
+
+  });
+  
