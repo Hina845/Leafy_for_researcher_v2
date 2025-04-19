b9abcfe495d010c657cb66664abae6078d933075
RealDangTan <11236199@st.neu.edu.vn>
2025-04-19 15:15:35 +0700

    done 7-2, missing tags

--- public/scripts/component.js
+++ public/scripts/component.js
@@ -11,11 +11,8 @@ function loadComponent(selector, file) {
   document.addEventListener("DOMContentLoaded", () => {
     loadComponent("#navigation", "../components/navigation.html");
     loadComponent("#footer", "../components/footer.html");
-<<<<<<< HEAD:public/scripts/component.js
     loadComponent('#search-panel', '../components/search-panel.html');
     loadComponent('.profile-card', '../components/profile-card.html');
     loadComponent('.content-card', '../components/content-card.html');
-=======
->>>>>>> main:public/scripts/index.js
   });
   
